4da0f0399a0cc12028c9a8e4f6984273902f4ebe
Fayaz Ahmed <fayazara@gmail.com>
2024-07-17 09:55:15 +0530

    Handle errors

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,22 +2,31 @@ import { validateBlob } from "~/utils/blob";
 import { FileStorage } from "~/services/storage";
 
 export default defineEventHandler(async (event) => {
-  const form = await readFormData(event);
-  const image = form.get("image");
+  try {
+    const form = await readFormData(event);
+    const image = form.get("image");
 
-  if (!(image instanceof Blob)) {
-    throw new Error("Image is not a Blob");
+    if (!(image instanceof Blob)) {
+      throw new Error("Image is not a Blob");
+    }
+    validateBlob(image, {
+      maxSize: "1MB",
+      types: ["image/png", "image/jpeg", "image/webp"],
+    });
+
+    const imageId = Math.random().toString(36).substring(2, 15);
+    const uniqueKey = `${imageId}-${image.name}`;
+
+    const storageService = new FileStorage();
+    await storageService.upload(uniqueKey, image);
+    const publicUrl = storageService.getObjectUrl(uniqueKey);
+    return publicUrl;
+  } catch (error) {
+    console.error(error);
+    throw createError({
+      statusCode: 400,
+      statusMessage: "Bad Request",
+      message: error.message,
+    });
   }
-  validateBlob(image, {
-    maxSize: "1MB",
-    types: ["image/png", "image/jpeg", "image/webp"],
-  });
-
-  const imageId = Math.random().toString(36).substring(2, 15);
-  const uniqueKey = `${imageId}-${image.name}`;
-
-  const storageService = new FileStorage();
-  await storageService.upload(uniqueKey, image);
-  const publicUrl = storageService.getObjectUrl(uniqueKey);
-  return publicUrl;
 });
